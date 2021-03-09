3d087e405d17569469acdf8dc2da174d9ee824d3
alex-treebeard <alex@treebeard.io>
2021-03-09 17:55:10 +0000

    rm deps

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,8 +1,4 @@
 import * as assert from 'assert'
-import {createConnection} from 'typeorm'
-import 'reflect-metadata'
-
-import {User} from '../../entity/User'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -12,29 +8,8 @@ import * as vscode from 'vscode'
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
 
-  // test('Sample test', () => {
-  //   assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-  //   assert.strictEqual(-1, [1, 2, 3].indexOf(0))
-  // })
-
-  test('DB', async () => {
-    var sqlite3 = require('sqlite3').verbose()
-    var db = new sqlite3.Database('/Users/a/git/treebeardtech/deeptest/debug/src/.coverage')
-
-    db.serialize(function () {
-      db.run('CREATE TABLE lorem (info TEXT)')
-
-      var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-      for (var i = 0; i < 10; i++) {
-        stmt.run('Ipsum ' + i)
-      }
-      stmt.finalize()
-
-      db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-        console.log(row.id + ': ' + row.info)
-      })
-    })
-
-    db.close()
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5))
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0))
   })
 })
