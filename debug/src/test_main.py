171313e8e49928510983805fe0cf033208d4f2ce
alex-treebeard <alex@treebeard.io>
2021-03-09 09:50:53 +0000

    add debug files

---
+++ debug/src/test_main.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    return x + y
+
+def test_add():
+    assert add(1,2) == 3
+
+def test_add2():
+    assert add(1,2) == 3
+
--- debug/test.js
+++
@@ -1,4 +0,0 @@
-console.log()
-console.log()
-fsdfs
-lk
