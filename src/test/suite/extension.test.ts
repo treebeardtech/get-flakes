d15ad33f25135566d12b22f7cb5e293c39b9ae71
alex-treebeard <alex@treebeard.io>
2021-03-09 11:01:25 +0000

    orm not working

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,8 @@
 import * as assert from 'assert'
+import {createConnection} from 'typeorm'
+import 'reflect-metadata'
+
+import {User} from '../../entity/User'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -6,10 +10,28 @@ import * as vscode from 'vscode'
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.')
+  vscode.window.showInformationMessage('Start all tests.')
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5))
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0))
-	})
+  // test('Sample test', () => {
+  //   assert.strictEqual(-1, [1, 2, 3].indexOf(5))
+  //   assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+  // })
+
+  test('DB', async () => {
+    const connection = await createConnection()
+    console.log('Inserting a new user into the database...')
+    throw Error('boom')
+    // const user = new User()
+
+    // user.firstName = 'Timber'
+
+    // user.lastName = 'Saw'
+    // user.age = 25
+    // await connection.manager.save(user)
+    // console.log(`Saved a new user with id: ${user.id}`)
+
+    // console.log('Loading users from the database...')
+    // const users = await connection.manager.find(User)
+    // console.log('Loaded users: ', users)
+  })
 })
