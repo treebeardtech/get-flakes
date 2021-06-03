04e17648fd37c650dbe0302c759d5eb0fedd780a
Alex Remedios <alex@treebeard.io>
2021-06-03 14:35:53 +0100

    uploader (#14)

--- python-cli/tests/test_cli.py
+++ python-cli/tests/test_cli.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+
+from click.testing import CliRunner
+from deeptest import cli
+
+pytest_plugins = "pytester"
+
+
 class TestCli:
-    def test_when_test_file_then_success(self):
-        pass
+    def test_a(self, testdir):
+        Path("junit.xml").write_text("lkj")
+        runner = CliRunner()
+        result = runner.invoke(cli.run, f"junit.xml", catch_exceptions=False)
+        print(result.output)
