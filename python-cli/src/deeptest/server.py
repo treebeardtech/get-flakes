0fe56321fba33c10c734f6ba1364da2b8f22fda8
alex-treebeard <alex@treebeard.io>
2021-06-03 11:58:24 +0100

    fastapi

---
+++ python-cli/src/deeptest/server.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
