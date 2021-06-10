d4ccda0894e26c86bec441cc5618dba6f8e3f0f7
alex-treebeard <alex@treebeard.io>
2021-06-10 21:22:19 +0100

    parse

--- src/get_flakes/github.py
+++ src/get_flakes/github.py
@@ -1,31 +1,48 @@
 from typing import List
+
 from pydantic import BaseModel
+
 
 class App(BaseModel):
     name: str
+
 
 class CheckRun(BaseModel):
     databaseId: str
     conclusion: str
     name: str
 
+
 class CheckRuns(BaseModel):
     nodes: List[CheckRun]
-    
+
+
 class CheckSuite(BaseModel):
     app: App
     checkRuns: CheckRuns
 
+
 class CheckSuites(BaseModel):
     nodes: List[CheckSuite]
+
 
 class Commit(BaseModel):
     oid: str
     checkSuites: CheckSuites
 
+
 class Commits(BaseModel):
-    nodes: List[Commit]
+    commit: List[Commit]
+
+
+class PRCommit(BaseModel):
+    commit: Commit
+
+
+class PRCommits(BaseModel):
+    nodes: List[PRCommit]
+
 
 class PullRequest(BaseModel):
     number: int
-    commits: Commits
+    commits: PRCommits
